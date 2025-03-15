eb7a52d3ffd6580cd562b479d5a88c60a91c7781
farrrahhh <18222096@std.stei.itb.ac.id>
2025-03-16 01:05:16 +0700

    No code changes detected.

---
+++ my-app/App.tsx
@@ -0,0 +1,10 @@
+import './global.css';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex h-full items-center justify-center">
+      <Text className="text-xl text-white">Hello World!!</Text>
+    </View>
+  );
+}
