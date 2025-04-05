921d03cc3cc1d302f131f89dfa0c0ef2d857bdd0
farrrahhh <18222096@std.stei.itb.ac.id>
2025-04-05 15:20:35 +0700

    update import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs';
 
 // Import models dari CommonJS
 import { sequelize, UserWheelchair, UserGuardian } from './models.js';
